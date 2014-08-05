cdd649821176fa23fb3a77748c15a7113add677f
Kenji Miwa <kenjim83@gmail.com>
2014-08-05 15:54:13 -0700

    git ignore

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def google_auth
-    debugger
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path
