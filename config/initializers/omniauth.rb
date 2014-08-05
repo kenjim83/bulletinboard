7faf942a1c9c076a2fc4a2ff43e8acd037525978
Kenji <kenjim83@gmail.com>
2014-08-05 15:53:27 -0700

    Delete omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '228927113824-mpehv945iiuvpcopskvog168sl5gnhtp.apps.googleusercontent.com', 'FnE9mNPl_iZ4NO8M9USXxbxS', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
